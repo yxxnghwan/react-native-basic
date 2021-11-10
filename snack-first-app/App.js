83fe4ed5dbad10ea48ba2cb97a707fbf82bd2f3b
yxxnghwan <younghwan960@gmail.com>
2021-11-10 14:57:51 +0900

    first react-native-app Clock

---
+++ snack-first-app/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import styled from 'styled-components/native';
+import {DateTime} from 'luxon';
+import ClockDigital from './screens/ClockDigital'
+
+export default function App() {
+  
+  return (
+    <ClockDigital/>
+  );
+}
+
+
---
+++ snack-first-app/components/Second.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+const Second = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  background: rgba(255, 255, 255, 0.4);
+  margin-top : 20px;
+`;
+
+export default Second;
